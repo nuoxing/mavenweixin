e09f2167f4b846f920aee386be4277288b3ef962
suwy <1573243972@qq.com>
2018-01-18 14:27:51 +0800

    初始化导入

---
+++ src/main/java/com/quartz/GetAccessTokenQuartz.java
@@ -0,0 +1,61 @@
+/*
+ * 文件名：GetAccessTokenQuartz.java 版权：Copyright by www.chinauip.com 描述： 修改人：Administrator
+ * 修改时间：2018年1月17日 跟踪单号： 修改单号： 修改内容：
+ */
+
+package com.quartz;
+
+
+import org.springframework.stereotype.Component;
+
+import com.weixin.service.JsSdkService;
+import com.weixin.util.ConstantUtils;
+import com.weixin.util.HttpClientUtils;
+
+import net.sf.json.JSONException;
+import net.sf.json.JSONObject;
+
+
+/**
+ * 获取ccessToken的定时任务 〈功能详细描述〉
+ * 
+ * @author suwy
+ * @version 2018年1月17日
+ * @see GetAccessTokenQuartz
+ * @since
+ */
+@Component("getAccessTokenQuartz")
+public class GetAccessTokenQuartz
+{
+
+    public void getAccessToken()
+    {
+        System.out.println("开始获取AccessToken");
+        String res = HttpClientUtils.commonGet(ConstantUtils.ACCESS_TOKEN_URL, "utf-8");
+        if (!"error".equals(res))
+        {
+            JSONObject json;
+            try
+            {
+                json =JSONObject.fromObject(res);
+                if (json.getString("access_token") != null)
+                {
+                    ConstantUtils.ACCESS_TOKEN = json.getString("access_token");
+                    
+                    //
+                    res = HttpClientUtils.commonGet(ConstantUtils.getJsapiTicketUrl(), "utf-8");
+                    json = JSONObject.fromObject(res);
+                    if (json.getString("errmsg").equals("ok")){
+                        JsSdkService.jsapi_ticket = json.getString("ticket");
+                    }
+                }
+            }
+            catch (JSONException e)
+            {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+}
